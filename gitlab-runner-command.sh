1206ede3be7119a514dacab3bdaa39648ca85953
KotDimos <KotDimos@gmail.com>
2023-01-02 21:10:07 +0300

    feat: add variable for container name

--- gitlab-runner-command.sh
+++ gitlab-runner-command.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+source .env
+
 if [[ -z $1 ]]
 then
     echo "gitlab-runner: command not found"
     exit 1
 fi
 
-docker exec -it gitlab-runner gitlab-runner $1
-
+docker exec -it ${DOCKER_CONTAINER_NAME} gitlab-runner $1
